3938a149df32a512974d601541e6e5642d5df7dd
Takagi Kensuke <janus.wel.3@gmail.com>
2025-04-28 15:38:38 +0900

    ✨ implement roughly

--- packages/textlint-rule-kanji-needs-ruby/src/index.ts
+++ packages/textlint-rule-kanji-needs-ruby/src/index.ts
@@ -1,2 +1,43 @@
-// TODO: 実装は後で追加
-export default {};
+import { TextlintRuleModule } from "@textlint/types";
+
+// ルビ要素の範囲を取得
+function getRubyRanges(text: string): [number, number][] {
+  const ranges: [number, number][] = [];
+  // <ruby>タグ全体を取得
+  const rubyRegex = /<ruby>[\s\S]*?<\/ruby>/g;
+  let match;
+  while ((match = rubyRegex.exec(text)) !== null) {
+    // <rt>要素が含まれている場合のみルビ範囲とみなす
+    if (/<rt>[\s\S]*?<\/rt>/.test(match[0])) {
+      ranges.push([match.index, match.index + match[0].length]);
+    }
+  }
+  return ranges;
+}
+
+// その位置がルビ範囲内か判定
+function isInRubyRanges(index: number, ranges: [number, number][]) {
+  return ranges.some(([start, end]) => index >= start && index < end);
+}
+
+const rule: TextlintRuleModule = (context) => {
+  const { Syntax, getSource, report, RuleError } = context;
+  return {
+    [Syntax.Paragraph](node) {
+      const text = getSource(node);
+      const rubyRanges = getRubyRanges(text);
+      for (let i = 0; i < text.length; i++) {
+        const char = text[i];
+        // すべての漢字かつルビ範囲外
+        if (/\p{Script=Han}/u.test(char) && !isInRubyRanges(i, rubyRanges)) {
+          report(
+            node,
+            new RuleError(`「${char}」にルビを振ってください`, { index: i })
+          );
+        }
+      }
+    },
+  };
+};
+
+export default rule;
--- packages/textlint-rule-kanji-needs-ruby/test/index.ts
+++ packages/textlint-rule-kanji-needs-ruby/test/index.ts
@@ -1,1 +1,28 @@
 // TODO: テスト実装は後で追加
+import TextLintTester from "textlint-tester";
+import rule from "../src/index";
+
+const tester = new TextLintTester();
+
+tester.run("kanji-needs-ruby", rule, {
+  valid: [
+    // ルビが必要な漢字にルビが付いている例
+    "これは<ruby>漢字<rt>かんじ</rt></ruby>です。",
+    "- リストでも<ruby>検出<rt>検出</rt></ruby>します。",
+    "ルビ<ruby>非対応<rp>(</rp><rt>ひたいおう</rt><rp>)</rp></ruby>ビューアーもいけます。",
+  ],
+  invalid: [
+    {
+      // ルビが必要な漢字にルビが付いていない例
+      text: "これは漢字です。",
+      errors: [
+        {
+          message: "「漢」にルビを振ってください",
+        },
+        {
+          message: "「字」にルビを振ってください",
+        },
+      ],
+    },
+  ],
+});
