193dd511cbf88bface2b49aee8deb3e1f19a29dd
Takagi Kensuke <janus.wel.3@gmail.com>
2025-04-28 12:36:20 +0900

    🍱 define kanjis that are treated in elementary school # gitmoji template to complete # # 🎨 - :art: - Improve structure / format of the code. # ⚡️ - :zap: - Improve performance. # 🔥 - :fire: - Remove code or files. # 🐛 - :bug: - Fix a bug. # 🚑️ - :ambulance: - Critical hotfix. # ✨ - :sparkles: - Introduce new features. # 📝 - :memo: - Add or update documentation. # 🚀 - :rocket: - Deploy stuff. # 💄 - :lipstick: - Add or update the UI and style files. # 🎉 - :tada: - Begin a project. # ✅ - :white_check_mark: - Add, update, or pass tests. # 🔒️ - :lock: - Fix security or privacy issues. # 🔐 - :closed_lock_with_key: - Add or update secrets. # 🔖 - :bookmark: - Release / Version tags. # 🚨 - :rotating_light: - Fix compiler / linter warnings. # 🚧 - :construction: - Work in progress. # 💚 - :green_heart: - Fix CI Build. # ⬇️ - :arrow_down: - Downgrade dependencies. # ⬆️ - :arrow_up: - Upgrade dependencies. # 📌 - :pushpin: - Pin dependencies to specific versions. # 👷 - :construction_worker: - Add or update CI build system. # 📈 - :chart_with_upwards_trend: - Add or update analytics or track code. # ♻️ - :recycle: - Refactor code. # ➕ - :heavy_plus_sign: - Add a dependency. # ➖ - :heavy_minus_sign: - Remove a dependency. # 🔧 - :wrench: - Add or update configuration files. # 🔨 - :hammer: - Add or update development scripts. # 🌐 - :globe_with_meridians: - Internationalization and localization. # ✏️ - :pencil2: - Fix typos. # 💩 - :poop: - Write bad code that needs to be improved. # ⏪️ - :rewind: - Revert changes. # 🔀 - :twisted_rightwards_arrows: - Merge branches. # 📦️ - :package: - Add or update compiled files or packages. # 👽️ - :alien: - Update code due to external API changes. # 🚚 - :truck: - Move or rename resources (e.g.: files, paths, routes). # 📄 - :page_facing_up: - Add or update license. # 💥 - :boom: - Introduce breaking changes. # 🍱 - :bento: - Add or update assets. # ♿️ - :wheelchair: - Improve accessibility. # 💡 - :bulb: - Add or update comments in source code. # 🍻 - :beers: - Write code drunkenly. # 💬 - :speech_balloon: - Add or update text and literals. # 🗃️ - :card_file_box: - Perform database related changes. # 🔊 - :loud_sound: - Add or update logs. # 🔇 - :mute: - Remove logs. # 👥 - :busts_in_silhouette: - Add or update contributor(s). # 🚸 - :children_crossing: - Improve user experience / usability. # 🏗️ - :building_construction: - Make architectural changes. # 📱 - :iphone: - Work on responsive design. # 🤡 - :clown_face: - Mock things. # 🥚 - :egg: - Add or update an easter egg. # 🙈 - :see_no_evil: - Add or update a .gitignore file. # 📸 - :camera_flash: - Add or update snapshots. # ⚗️ - :alembic: - Perform experiments. # 🔍️ - :mag: - Improve SEO. # 🏷️ - :label: - Add or update types. # 🌱 - :seedling: - Add or update seed files. # 🚩 - :triangular_flag_on_post: - Add, update, or remove feature flags. # 🥅 - :goal_net: - Catch errors. # 💫 - :dizzy: - Add or update animations and transitions. # 🗑️ - :wastebasket: - Deprecate code that needs to be cleaned up. # 🛂 - :passport_control: - Work on code related to authorization, roles and permissions. # 🩹 - :adhesive_bandage: - Simple fix for a non-critical issue. # 🧐 - :monocle_face: - Data exploration/inspection. # ⚰️ - :coffin: - Remove dead code. # 🧪 - :test_tube: - Add a failing test. # 👔 - :necktie: - Add or update business logic. # 🩺 - :stethoscope: - Add or update healthcheck. # 🧱 - :bricks: - Infrastructure related changes. # 🧑‍💻 - :technologist: - Improve developer experience. # 💸 - :money_with_wings: - Add sponsorships or money related infrastructure. # 🧵 - :thread: - Add or update code related to multithreading or concurrency. # 🦺 - :safety_vest: - Add or update code related to validation.

--- packages/textlint-rule-japanese-elementary-school-kanji/index.ts
+++ packages/textlint-rule-japanese-elementary-school-kanji/index.ts
@@ -6,7 +6,7 @@
 import kanjisJson from "./kanjis.json";
 import type { TextlintRuleModule } from "@textlint/types";
 
-const kanjis = new Set<string>(kanjisJson);
+const kanjis = new Set<string>(Object.values(kanjisJson).flat());
 
 const rule: TextlintRuleModule = (context) => {
   const { Syntax, getSource, RuleError, report } = context;
--- packages/textlint-rule-japanese-elementary-school-kanji/test.ts
+++ packages/textlint-rule-japanese-elementary-school-kanji/test.ts
@@ -7,11 +7,10 @@ tester.run("japanese-elementary-school-kanji", rule, {
   valid: ["今日はいい天気です。", "私は学生です。"],
   invalid: [
     {
-      // すべて小学生で習う範囲外の漢字
+      // 「優」以外は小学生で習う範囲外の漢字
       text: "彼は優秀です。",
       errors: [
         { message: "「彼」は小学校で習わない漢字です。" },
-        { message: "「優」は小学校で習わない漢字です。" },
         { message: "「秀」は小学校で習わない漢字です。" },
       ],
     },
