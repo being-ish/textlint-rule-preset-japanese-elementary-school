e90cbf3b0f716a074abc40c715e1b76d2a5e20f2
Takagi Kensuke <janus.wel.3@gmail.com>
2025-04-29 22:08:36 +0900

    🔧 use textlint-scripts

---
+++ packages/textlint-rule-preset-japanese-elementary-school/src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+module.exports = {
+  rules: {
+    "japanese-elementary-school-kanji": require("textlint-rule-japanese-elementary-school-kanji"),
+    "kanji-needs-ruby": require("textlint-rule-kanji-needs-ruby"),
+  },
+};
